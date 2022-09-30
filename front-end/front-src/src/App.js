bc09cb1a20cd299dcc4eb169eefba5346fecb75d
Simeon Tran <siatran@ucsc.edu>
2022-09-30 11:37:04 -0700

    Got a button to print some text to the console in React

--- front-end/front-src/src/App.js
+++ front-end/front-src/src/App.js
@@ -1,23 +1,25 @@
+/* ---- SOURCES USED ----
+ * - https://www.robinwieruch.de/react-event-handler/
+ * - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+ * - https://reactjs.org/docs/handling-events.html
+*/
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const talkToServer = function() {
+    console.log("Button Clicked!");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Press the button to get the server to do something.</p>
+
+      <button onClick = {talkToServer}>
+        Press Me!
+      </button>
     </div>
   );
 }
