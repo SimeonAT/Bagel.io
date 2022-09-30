e27ddd9371ccaa785e877ef3098a36444d3f83f4
Simeon Tran <siatran@ucsc.edu>
2022-09-30 11:47:48 -0700

    Wrote front-end for sending basic response to server

--- front-end/front-src/src/App.js
+++ front-end/front-src/src/App.js
@@ -2,22 +2,35 @@
  * - https://www.robinwieruch.de/react-event-handler/
  * - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
  * - https://reactjs.org/docs/handling-events.html
+ * - https://www.digitalocean.com/community/tutorials/how-to-write-asynchronous-code
+ *   -in-node-js
+ * - https://www.stackhawk.com/blog/react-cors-guide-what-it-is-and-how-to-enable-it/
+ * - https://developer.mozilla.org/en-US/docs/Web/API/Response
 */
 
 import logo from './logo.svg';
 import './App.css';
 
+const BACKEND_URL = "";
+
 function App() {
 
-  const talkToServer = function() {
-    console.log("Button Clicked!");
+  const talk_to_server = async function() {
+    let http_response = await fetch(BACKEND_URL, {
+      mode: "cors"
+    });
+
+    let response_body = await http_response.data();
+
+    console.log(response_body);
+    return;
   }
 
   return (
     <div>
       <p>Press the button to get the server to do something.</p>
 
-      <button onClick = {talkToServer}>
+      <button onClick = {talk_to_server}>
         Press Me!
       </button>
     </div>
