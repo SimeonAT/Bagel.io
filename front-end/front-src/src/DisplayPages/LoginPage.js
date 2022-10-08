4981ea1c2dc7e106fcf6d7b0d8371a9857a2765f
Kalani Richard <kalanirichard@Kalanis-MacBook-Pro.local>
2022-10-07 20:40:06 -0700

    used mui to make prettier

--- front-end/front-src/src/DisplayPages/LoginPage.js
+++ front-end/front-src/src/DisplayPages/LoginPage.js
@@ -1,14 +1,46 @@
-import React, { useState } from "react";
-//import ReactDOM from "react-dom";
+import {useState} from "react";
+import * as React from 'react';
+//import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+//import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import '../Styles.css';
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Bagel.io
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-function LoginPage() {
-  // React States
+const theme = createTheme( {
+  palette: {
+    primary: {
+      light: '#d1ccdc',
+      main: '##886f68',
+      dark: '#424c55',
+      contrastText: '#fff',
+    },
+  },
+  });
+
+export default function SignIn() {
   const [errorMessage, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  // User Login info
   const database = [
     {
       username: "user1",
@@ -24,16 +56,11 @@ function LoginPage() {
     u_name: "invalid username",
     pass: "invalid password"
   };
-
   const handleSubmit = (event) => {
     event.preventDefault();
-
     var { u_name, pass } = document.forms[0];
+    const userData = database.find((user) => user.username === u_name.value)
 
-    // Find user login info
-    const userData = database.find((user) => user.username === u_name.value);
-
-    // Check user info
     if (userData) {
       if (userData.password !== pass.value) {
         // pass doesnt match
@@ -46,42 +73,76 @@ function LoginPage() {
       setErrorMessages({ name: "u_name", message: errors.u_name });
     }
   };
-
-  // error message
+  
   const renderErrorMessage = (name) =>
-    name === errorMessage.name && (
-      <div className="error">{errorMessage.message}</div>
-    );
-
-  //login form
+  name === errorMessage.name && (
+    <div className="error">{errorMessage.message}</div>
+  );
   const renderForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>Username </label>
-          <input type="userName" name="u_name" required />
+    <ThemeProvider theme={theme}>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="u_name"
+            //autoComplete="email"
+            autoFocus
+            
+          />
           {renderErrorMessage("u_name")}
-        </div>
-        <div className="input-container">
-          <label>Password </label>
-          <input type="password" name="pass" required />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="pass"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
           {renderErrorMessage("pass")}
-        </div>
-        <div className="submitButtonContainer">
-          <input type="submit" />
-        </div>
-      </form>
-    </div>
-  );
 
-  return (
-    <div className="Login">
-      <div className="login-form">
-        <div className="title">Sign In</div>
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
-      </div>
+
+          <Button
+            color="primary"
+            type="submit"
+            fullWidth
+            
+            variant="outlined"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+
+        </Box>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
+  </ThemeProvider>
+);
+return (
+  <div className="Login">
+    <div className="login-form">
+      <div className="title"></div>
+      {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
     </div>
-  );
+  </div>
+);
 }
-
-export default LoginPage;
