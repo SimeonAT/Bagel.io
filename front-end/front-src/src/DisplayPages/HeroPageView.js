73e2af696fb39652c46d0ff9bec3ce67f7a02cfb
Simeon Tran <siatran@ucsc.edu>
2022-10-08 23:07:05 -0700

    Going to redo welcome page

--- front-end/front-src/src/DisplayPages/HeroPageView.js
+++ front-end/front-src/src/DisplayPages/HeroPageView.js
@@ -28,24 +28,7 @@ const REGISTER = 1;
 const LOGIN = 2;
 
 function WelcomeView() {
-  return (
-    <div>
-      <div className = "title-section">
-        <div className="title-capital-letter-element">
-          <div className="title-capital-letter-div">B</div>
-        </div>
-        <div className="secondary-title-element">
-          <div className="title-lowercase-letter">agel</div>
-          <div className="subtitle">take back your time</div> 
-        </div>
-      </div> 
-      <div className = "list-section">
-        <ul>• Plan</ul>
-        <ul>• Prioritize</ul>
-        <ul>• Perfect</ul>
-      </div>
-    </div>
-  );
+  return (null);
 }
 
 function HeroView() { 
@@ -56,7 +39,9 @@ function HeroView() {
   */
   const changeView = function() {
     if (userStatus === WELCOME) {
-      return (<WelcomeView />);
+      return (
+      <WelcomeView />
+      );
     }
     else if (userStatus === LOGIN) {
       return (null);
@@ -97,24 +82,7 @@ function HeroView() {
     return;
   }
 
-  return (
-    <section className="hero-view-frame-element">
-
-    <div className="hero-view-content">
-      <div className="login-button-element">
-        <button className="login-button" 
-          onClick = {loginHandler}>Login</button>
-      </div> 
-
-      {changeView()}
-
-      <div className="create-button-element">
-        <button className="create-account-button"
-          onClick = {registerHandler}>Make your account now!</button>
-      </div>  
-    </div>
-  </section>
-  );
+  return (null);
 }
 
 export default HeroView;
