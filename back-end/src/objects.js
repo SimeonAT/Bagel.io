7db8eba53a084cd50f9a2cc6f9fc38d9529c9faa
thestrongest9 <gene.jaewoo@gmail.com>
2022-10-19 11:58:33 -0700

    Mock back-end in taskList() class

--- back-end/src/objects.js
+++ back-end/src/objects.js
@@ -1,10 +1,11 @@
 
 
-class user {
-	constructor() {
-		this.username;
-		this.password;
-		this.tasks = [];
+export class user {
+	constructor(username, password, email, tasks) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.tasks = tasks;
 	}
 
 	authenticate() {
@@ -17,9 +18,9 @@ class user {
 
 };
 
-class task {	
-	constructor() {
-		this.name;
+export class task {	
+	constructor(name) {
+		this.name = name;
 		this.startTime;
 		this.endTime;
 		this.duration;
@@ -39,3 +40,69 @@ class task {
 		this.duration = this.endTime - this.startTime;
 	}
 };
+
+export class taskList() {
+	//foobar TIMESTAMP WITH TIME ZONE
+	//member USER
+	//task preset 
+	//task scheduled
+	constructor() {
+		this.userObj; //member
+		this.taskPresets = {}; //task presets
+		this.taskScheduled = {}; //task scheduled
+	}
+
+	setUser(username, password, email, tasks=[]) {
+		this.userObj = new user(username, password, email, tasks);
+		//check format of tasks before adding it to constructor???
+	}
+
+	addTaskPreset(name, startTime) { //arguments should include task stuff.
+		//please have startTime in ISO standard.
+		var newTask = new task(name);
+		var taskID = new Date(startTime);
+		this.taskPresets[taskID] = newTask;
+		return taskID; //Unique ID
+		//return taskID
+	}
+
+	scheduleTask(startTime, endTime, taskID) { //have start and end times in ISO
+		//get the task preset and clone it
+		var clonedPreset = structuredClone(this.taskPresets[taskID]);
+		clonedPreset.setStartTime(startTime); //set start and end times
+		clonedPreset.setEndTime(endTime);
+		var taskID = new Date(startTime);
+		this.taskScheduled[taskID] = clonedPreset; //put it into scheduled tasks
+		return taskID; //taskID = startTime.
+	}
+
+	returnScheduleTask(taskID) {
+		if (!(taskID in this.taskScheduled)) {
+			return; //task does not exist in presets.
+		}
+		return this.taskScheduled[taskID]; //if exists, returns entire task object.
+	}
+
+	editScheduleTask(startTime, endTime, taskID) {
+		if (!(taskID in this.taskScheduled)) {
+			return false; //task does not exist in presets.
+		} 
+		this.taskScheduled[taskID].startTime = startTime;
+		this.taskScheduled[taskID].endTime = endTime;
+		return true; //edits added.
+	}
+
+	returnListScheduleTask() {
+		return this.taskScheduled; //returns all tasks scheduled.
+	}
+
+	//set task complete or incomplete
+	setComplete(taskID, done) {
+		this.taskScheduled[taskID].completed = done;
+	}
+
+	//get task complete or incomplete
+	getComplete(taskID) {
+		return this.taskScheduled[taskID].completed;
+	}
+}
