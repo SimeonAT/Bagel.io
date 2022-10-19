7f74ad46f3a10c40bd82f375e434ebc50da62d38
thestrongest9 <gene.jaewoo@gmail.com>
2022-10-19 01:16:23 -0700

    objects.js file for task objects. Should be the same file for FRONT and BACK end.

---
+++ front-end/front-src/src/objects.js
@@ -0,0 +1,39 @@
+class task {
+	constructor() {
+		this.name;
+		this.startTime;
+		this.endTime;
+		this.duration;
+		this.date;
+		this.tag;
+	}
+
+	setDate() {
+		var currentDate = new Date();
+		//year, month, day, hours, minutes, seconds, in UTC time.
+		this.date = currentDate.getUTCFullYear() + "-"
+			    currentDate.getUTCMonth() + "-"
+			    currentDate.getUTCDate() + "-"
+			    currentDate.getUTCHours() + "-"
+			    currentDate.getUTCMinutes() + "-"
+			    currentDate.getUTCSeconds();
+	}
+
+	setStartTime() {
+		var temp = new Date();
+		var startDate = temp.getTime();
+		this.startTime = startDate; //returns miliseconds since January 1st, 1970, 00:00:00 UTC.
+	}
+
+	markEndTime(hours, minutes) { //this is for manually setting how long the task took.
+		this.endTime = hours * 3600000 + minutes * 60000;// + seconds * 1000; saves in milliseconds.
+		this.duration = this.endTime - this.startTime; //setting end time also sets duration.
+	}
+
+	setEndTime() { //for start-stop timer.
+		var temp = new Date();
+		var endDate = temp.getTime();
+		this.endTime = endDate;
+		this.duration = this.endTime - this.startTime;
+	}
+};
