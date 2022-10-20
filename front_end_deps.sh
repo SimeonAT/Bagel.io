8666ceb59aa75bf6c9ea81b294c43f541c9b886b
Simeon Tran <siatran@ucsc.edu>
2022-10-20 09:34:11 -0700

    Added Day.js dependices to sh file

--- front_end_deps.sh
+++ front_end_deps.sh
@@ -3,3 +3,5 @@ cd front-end/front-src
 npm install --save react-router-dom
 npm install @mui/material @emotion/react @emotion/styled
 npm install --save styled-components
+npm install @mui/x-date-pickers
+npm install dayjs @date-io/dayjs
