9c2d30b42fb715c0c18a1cf468278ad535a773d9
Simeon Tran <siatran@ucsc.edu>
2022-10-26 10:26:50 -0700

    Removed eslint

--- front-end/front-src/src/Main.test.js
+++ front-end/front-src/src/Main.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import Main from './Main';
 
 test('renders learn react link', () => {
